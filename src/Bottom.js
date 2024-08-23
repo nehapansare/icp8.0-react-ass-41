74f3fc62349cd604976ec2d50fbaa63c77681456
nehapansare <nehapansare007@gmail.com>
2024-08-23 11:43:44 +0530

    react-router-dom  added

--- src/Bottom.js
+++ src/Bottom.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import bottom from './Bottom.css'
+import './Bottom.css';
 
 function footer() {
   return (
     <div className='footer'>
       All right reserved &copy; 2024
+  
     </div>
+  
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
 
 
 import Home from "./Home";
@@ -8,17 +9,19 @@ import Contact from "./Contact";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const currentPath = window.location.pathname;
-if(currentPath === "/"){
-    root.render(<Home/>)
-}
-else if(currentPath === "/about"){
-    root.render(<About/>)
-}
-else if(currentPath === "/contact"){
-    root.render(<Contact/>)
-}
-else{
-    root.render(<h1>404</h1>)
-}
+const router =createBrowserRouter([
+    {
+        path:'/',
+        element: <Home/>,
+    },
+    {
+        path:'/about',
+        element:<About/>,
+    },
+    {
+        path:'/contact',
+        element:<Contact/>,
+    }
+])
+root.render(<RouterProvider router={router}/>)
 
